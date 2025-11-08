0ef1d801a283617054544cc2bb314f4575e7545a
Estudiante Ltic <estudiante@sistemas.local>
2025-11-08 12:49:23 -0500

    feat: script modification

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-console.log("Hola Mundo soy Jose Moya")
-
-var a = 1
-var b = 2
-
-var c = a+b
-
-console.log("El resultado es: ",c)
-
 function addClass(containerDiv, className){
     console.log(containerDiv.classlist)
     containerDiv.classList.add(className)
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    const containerDivs = document.querySelectorAll(".content")
-    console.log(containerDivs)
-    for (i=0; i < containerDivs.length;i++ ){
-        addClass(containerDivs[i], "juan")
+function createMatrix(dimension, container, divisorfizz, divisorBuzz){
+    let counter = 1
+
+    for(let i = 0;i < dimension; i++){
+        const div = document.createElement("div")
+        addClass(div, "demo")
+        for(let j= 0;j< dimension; j++){
+            const content = document.createElement("div")
+            addClass(content, "content")
+            fizz(divisorfizz, content, counter)
+            buzz(divisorBuzz, content, counter)
+            fizzbuzz(divisorfizz, divisorBuzz,content, counter)
+            content.textContent = counter
+            div.appendChild(content)
+            counter++
+        }
+        container.appendChild(div)
     }
+}
+
+function fizz(divisor, square, number){
+    if (number%divisor == 0){
+        addClass(square, "fizz")
+    } 
+}
+
+function buzz(divisor2, cube,  number){
+    if (number%divisor2 == 0){
+        addClass(cube, "buzz")
+    }
+}
+
+function fizzbuzz(divisor, divisor2, square_cube, number){
+    if (number%divisor == 0 && number%divisor2 == 0){
+        addClass(square_cube, "fizzbuzz")
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Ya se cargo este DOM")
+    const container = document.querySelector(".container")
+    createMatrix(5, container, 2, 3, )
 })
+
